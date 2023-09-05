3d5a259584d924a2b9c737b66a82ea92c4e02069
ANKIT KASHYAP <60142710+ANKIT-KASHYAP@users.noreply.github.com>
2023-09-05 22:28:40 +0530

    Delete Portfolio directory

--- Portfolio/index.js
+++
@@ -1,6 +0,0 @@
-function tooglemenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hanburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
